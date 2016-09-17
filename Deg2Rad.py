cac0cdba38f18e5f8f6d6680e1fe3e2b221f58f0
Adam <wiredmilk@gmail.com>
2016-09-17 15:28:55 -0700

    added quaternion

--- Deg2Rad.py
+++ Deg2Rad.py
@@ -1,26 +1,30 @@
 import math
 import csv
+from Quaternion import Quat
 
 with open('CartesianOut.csv', 'rt') as f:
     reader = csv.reader(f)
     outFile = open("ConvertedCartesianRadians.csv", 'w')
     writer = csv.writer(outFile, delimiter= ",")
-    writer.writerow(["Time","X","Y","Z","A","B","C","D","T"])
+    writer.writerow(["Time","X","Y","Z","A","B","C","D"])
     # writer = csv.duictWriter(outFile, fieldnames = ["Time","X","Y","Z","A","B","C","S","T"], delimiter= ",")
     # dictWriter.writeheader()
 
     next(reader, None)
 
     for row in reader:
-        a = float(row[4])
-        b = float(row[5])
-        c = float(row[6])
-        d = float(row[7])
+        # get vec3 rotation
+        x = float(row[4])
+        y = float(row[5])
+        z = float(row[6])
 
-        row[4] = math.radians(a)
-        row[5] = math.radians(b)
-        row[6] = math.radians(c)
-        row[7] = math.radians(d)
-        
-        writer.writerow(row)
-        print(a)
+        newQuat = Quat((x,y,z))
+
+        a = newQuat.q[0]
+        b = newQuat.q[1]
+        c = newQuat.q[2]
+        d = newQuat.q[3]
+
+        newRow = [row[0],row[1],row[2],row[3],a,b,c,d]
+        writer.writerow(newRow)
+        print newRow
